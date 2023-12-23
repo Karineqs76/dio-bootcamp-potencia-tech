5077bc886df2eb33e6bb43383227a4f38d37d4a5
Karine <138920893+Karineqs76@users.noreply.github.com>
2023-12-23 17:23:13 -0300

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,31 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+        switch (this.tipo) {
+            case 'mago':
+                ataque = 'usou magia';
+                break;
+            case 'guerreiro':
+                ataque = 'usou espada';
+                break;
+            case 'monge':
+                ataque = 'usou artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'usou shuriken';
+                break;
+            default:
+                ataque = 'atacou de forma genérica';
+        }
+
+        console.log(`o ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+const heroi1 = new Heroi('Gandalf', 1500, 'mago');
+heroi1.atacar();
